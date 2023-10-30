ce57f1e4af154d04542ef4e3cd30a846561d1ba4
KML23 <1kemalzth@gmail.com>
2023-10-30 15:13:29 +0700

    Second Commit

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Latihan 1 Modul 3");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan pertama: ");
+        int bilangan1 = input.nextInt();
+        
+        System.out.print("Masukkan bilangan kedua: ");
+        int bilangan2 = input.nextInt();
+        
+        int hasil = bilangan1 + bilangan2;
+        
+        System.out.println("Hasil penjumlahan: " + hasil);
     }
 }
